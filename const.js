9252579360487e83f163427e94714661a73fa557
jihun04 <yujihun20060217@gmail.com>
2020-11-08 23:04:09 +0900

    Update Errror Screen

--- const.js
+++ const.js
@@ -3,7 +3,13 @@ statusBar = body.querySelector(".status-bar"),
 screenHeader = body.querySelector(".screen-header"),
 nav = body.querySelector(".nav"),
 input = body.querySelector("input"),
-altScreenHeader = body.querySelector(".alt-header");
+altScreenHeader = body.querySelector(".alt-header"),
+battery1 = body.querySelector(".splash-screen1 .battery"),
+battery2 = body.querySelector(".splash-screen2 .battery"),
+battery3 = body.querySelector(".splash-screen3 .battery"),
+battery4 = body.querySelector(".splash-screen4 .battery"),
+battery5 = body.querySelector(".splash-screen5 .battery"),
+battery6 = body.querySelector(".splash-screen6 .battery");
 
 const BGCOLOR_LS = "bg-color",
 COLOR_LS = "color";
