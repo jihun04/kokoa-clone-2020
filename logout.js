20c61a32edbcbf98cc3fa3c63d38fd5530e75964
jihun04 <yujihun20060217@gmail.com>
2020-11-13 21:49:04 +0900

    Update logout.js

--- logout.js
+++ logout.js
@@ -3,11 +3,11 @@ const logoutBtn = document.querySelector(".logout-btn");
 const USERNAME_LS = "user-name";
 
 function handleLogOut() {
-    localStorage.removeItem(USERNAME_LS)
+    localStorage.removeItem(USERNAME_LS);
 }
 
 function init() {
-    logoutBtn.addEventListener("click", handleLogOut)
+    logoutBtn.addEventListener("click", handleLogOut);
 }
 
 init();
