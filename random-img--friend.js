6526aa61b4c6a67c847a94a4efe02d4ddf91d2d3
jihun04 <yujihun20060217@gmail.com>
2020-11-22 11:24:16 +0900

    Random Image, More Screen User Component

---
+++ random-img--friend.js
@@ -0,0 +1,28 @@
+const customUserComponent = document.querySelector(".user-component--custom"),
+customUserAvatarImg = customUserComponent.querySelector(".user-component__avatar--custom");
+
+const CUSTOMAVATARIMG_LS = "custom-avatar-img";
+
+function saveCustomAvatarImg(text) {
+    localStorage.setItem(CUSTOMAVATARIMG_LS, text);
+}
+
+function changeCustomImg() {
+    const randomNumber = genRandom();
+    customUserAvatarImg.src = `images/${randomNumber + 1}.jpg`;
+    saveCustomAvatarImg(customUserAvatarImg.src);
+}
+
+function loadAvatarImg() {
+    const currentCustomAvatarImg = localStorage.getItem(CUSTOMAVATARIMG_LS);
+    if(currentCustomAvatarImg !== null) {
+        customUserAvatarImg.src = currentCustomAvatarImg;
+    }
+}
+
+function init() {
+    loadAvatarImg();
+    customUserComponent.addEventListener("click", changeCustomImg);
+}
+
+init();
--- random-img.js
+++ random-img.js
@@ -1,11 +1,8 @@
 const userComponentOwn = document.querySelector(".user-component--own"),
-userAvatarImg = userComponentOwn.querySelector(".user-component__avatar--own"),
-customUserComponent = document.querySelector(".user-component--custom"),
-customUserAvatarImg = customUserComponent.querySelector(".user-component__avatar--custom");
+userAvatarImg = userComponentOwn.querySelector(".user-component__avatar--own");
 
 const IMG_NUMBER = 17,
-AVATARIMG_LS = "avatar-img",
-CUSTOMAVATARIMG_LS = "custom-avatar-img";
+AVATARIMG_LS = "avatar-img";
 
 function genRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
@@ -16,37 +13,22 @@ function saveAvatarImg(text) {
     localStorage.setItem(AVATARIMG_LS, text);
 }
 
-function saveCustomAvatarImg(text) {
-    localStorage.setItem(CUSTOMAVATARIMG_LS, text);
-}
-
 function changeOwnImg() {
     const randomNumber = genRandom();
     userAvatarImg.src = `images/${randomNumber + 1}.jpg`;
     saveAvatarImg(userAvatarImg.src);
 }
 
-function changeCustomImg() {
-    const randomNumber = genRandom();
-    customUserAvatarImg.src = `images/${randomNumber + 1}.jpg`;
-    saveCustomAvatarImg(customUserAvatarImg.src);
-}
-
 function loadAvatarImg() {
     const currentAvatarImg = localStorage.getItem(AVATARIMG_LS);
-    const currentCustomAvatarImg = localStorage.getItem(CUSTOMAVATARIMG_LS);
     if(currentAvatarImg !== null) {
         userAvatarImg.src = currentAvatarImg;
-    }
-    if(currentCustomAvatarImg !== null) {
-        customUserAvatarImg.src = currentCustomAvatarImg;
     }
 }
 
 function init() {
     loadAvatarImg();
     userComponentOwn.addEventListener("click", changeOwnImg);
-    customUserComponent.addEventListener("click", changeCustomImg);
 }
 
 init();
